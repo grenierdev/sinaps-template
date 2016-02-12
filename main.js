c09e25c361ed4341c1be4a9149245dd4f9997216
Michael Grenier <michael.grenier@live.com>
2016-02-12 12:19:06 -0500

    context, loop, if/else/if

--- main.js
+++ main.js
@@ -4,55 +4,61 @@ var util = require('util');
 var Environment = require('./lib/Environment');
 
 var str = `
-	Foo
-	{% macro foo %}
-		{{ 32 }}
-	{% endmacro %}
+	{{ foo }}
+	{{ goo.boo }}
+	{{ goo.boo.joo }}
+	{% if moo['zoo'].xoo == true %}
+		{{ coo[23].yoo }}
+		{{ coo[23].too(23) }}
+	{% endif %}
 
-	{% block foo %}
-		BLeh
-	{% endblock %}
+	{% for v in list %}
+		{{ loop.index0 }} / {{ loop.index }} : {{ v }} ({{ loop.first }} / {{ loop.last }})
 
-	{% for value in key %}
-		Yay
+		{% for k, v in dict %}
+			{{ loop.index0 }} / {{ loop.index }} : {{ k }} = {{ v }} ({{ loop.first }} / {{ loop.last }})
+		{% endfor %}
 	{% endfor %}
-
-	{% if true %}
-		Bleh
-	{% endif %}
-
-	{% if true %}
-		Mooo
-	{% else %}
-		Mehh
-	{% endif %}
-
-	{% if true %}
-		Mooo
-	{% elseif false %}
-		Mehh
-	{% endif %}
-
-	{% if true %}
-		Greee
-	{% elseif false %}
-		Juuu
-	{% else %}
-		Loooo
-	{% endif %}
 `;
 
 var env = new Environment();
-var template = env.compileString(str);
 
-console.log(
-	template({
-		foo: 'bar',
-		test: {
-			bar: function () { return 'foobar'; },
-			foo: {
-				bar: function () { return 'barfoo'; }
+var t = process.hrtime();
+var template = env.compileString(str);
+t = process.hrtime(t);
+var parsedTime = (t[0] * 1e9 + t[1]) / 1e6;
+
+t = process.hrtime();
+var output = template({
+	foo: 'Foo',
+	goo: {
+		boo: {
+			joo: 'Goo.Boo.Joo'
+		}
+	},
+	moo: {
+		zoo: {
+			xoo: true
+		}
+	},
+	coo: {
+		23: {
+			yoo: 'Coo.23.Yoo',
+			too: function (v) {
+				return `Coo.23.Too ${v}`;
 			}
 		}
-	})
-);
+	},
+	list: ['a', 'b'],
+	dict: {
+		a: 'Allo',
+		b: 'Bonjour',
+		c: 'Choulou'
+	}
+});
+t = process.hrtime(t)
+var outputTime = (t[0] * 1e9 + t[1]) / 1e6;
+
+console.log(`Parse : ${parsedTime}ms`);
+console.log(`Exec  : ${outputTime}ms`);
+console.log(output);
