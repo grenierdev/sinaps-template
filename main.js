ff30eddf70f101d1cf3875729658a09ccda23fb3
Michael Grenier <michael.grenier@live.com>
2016-02-14 15:04:41 -0500

    extends & import

--- main.js
+++ main.js
@@ -2,15 +2,20 @@
 
 var util = require('util');
 var Environment = require('./lib/Environment');
+var FileLoader = require('./lib/loaders/FileLoader');
 
 var str = `
 	{{ foo }}
 	{{ goo.boo }}
 	{{ goo.boo.joo }}
-	{% if moo['zoo'].xoo == true %}
+	{% if moo['zoo'].xoo %}
 		{{ coo[23].yoo }}
 		{{ coo[23].too(23) }}
 	{% endif %}
+	{{ {
+		a: 1,
+		b: 2
+	}.a }}
 
 	{% for v in list %}
 		{{ loop.index0 }} / {{ loop.index }} : {{ v }} ({{ loop.first }} / {{ loop.last }})
@@ -21,7 +26,36 @@ var str = `
 	{% endfor %}
 `;
 
-var env = new Environment();
+str = `
+{% extends "Bleh.html" %}
+{% macro foo %}
+	Blehhh
+{% endmacro %}
+
+{{ foo() }}
+`;
+
+str = `
+
+{% extends "a.html" %}
+
+This is main.js
+
+{% block foo %}
+	{{ "main.js !".toUpperCase() }}
+	{{ parent() }}
+{% endblock %}
+
+`;
+
+//str = `
+//{{ toString() }}
+//{{ "Bleh".toString() }}
+//{{ foo.bar.toString() }}
+//{{ foo.bar['toString']() }}
+//`;
+
+var env = new Environment(new FileLoader(__dirname + '/test'));
 
 var t = process.hrtime();
 var template = env.compileString(str);
@@ -55,10 +89,15 @@ var output = template({
 		b: 'Bonjour',
 		c: 'Choulou'
 	}
-});
+}).output;
 t = process.hrtime(t)
 var outputTime = (t[0] * 1e9 + t[1]) / 1e6;
 
+console.log('');
+console.log('');
+console.log('=======================');
 console.log(`Parse : ${parsedTime}ms`);
 console.log(`Exec  : ${outputTime}ms`);
+console.log('=======================');
 console.log(output);
+console.log('=======================');
