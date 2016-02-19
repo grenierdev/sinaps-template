d9ca0f0fa4db77165016b310542f38f5a2769914
Michael Grenier <michael.grenier@live.com>
2016-02-18 23:09:09 -0500

    fixes and MultiFileLoader

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 var Environment = require('./lib/Environment');
 var FileLoader = require('./lib/loaders/FileLoader');
+var MultiFileLoader = require('./lib/loaders/MultiFileLoader');
 
 module.exports = {
 	Environment: Environment,
-	FileLoader: FileLoader
+	FileLoader: FileLoader,
+	MultiFileLoader: MultiFileLoader
 }
