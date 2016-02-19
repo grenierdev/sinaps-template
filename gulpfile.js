901228768b2575efbeea6a1190027e40f209b0cd
Michael Grenier <michael.grenier@live.com>
2016-02-18 21:49:50 -0500

    gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,41 @@
 "use strict";
 
 var gulp = require('gulp');
-var browser = require('gulp-browser');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 var babel = require('gulp-babel');
 var rename = require('gulp-rename');
 var remove = require('del');
+var streamify = require('gulp-streamify')
 var uglify = require('gulp-uglify');
+var transform = require('vinyl-transform');
+var fs = require('fs');
+var peg = require('pegjs');
+var Stream = require('stream');
 
+gulp.task('parser:build', () => {
+	return gulp
+			.src('./lib/lexer.pegjs')
+			.pipe(transform(function (filename) {
+				return new Stream.Transform({
+					transform: function (chunk, encoding, done) {
+						var lexer = chunk.toString('utf8');
+						this.push(`module.exports = ` + peg.buildParser(lexer, {
+							cache: false,
+							optimize: 'speed',
+							output: 'source'
+						}));
+						done();
+					}
+				});
+			}))
+			.pipe(rename('Parser.js'))
+			.pipe(gulp.dest('./lib'))
+});
 
 gulp.task('browser:babel', () => {
 	return gulp
-			.src(['./browser.js', './lib/**/*.js'], {base: "."})
+			.src(['./main-browser.js', './lib/**/*.js'], {base: "."})
 			.pipe(babel({
 				presets: ['es2015']
 			}))
@@ -21,19 +46,38 @@ gulp.task('browser:babel-remove-fileloader', ['browser:babel'], () => {
 	return remove(['./tmp/lib/loaders/FileLoader.js']);
 });
 
-gulp.task('browser:browserify', ['browser:babel-remove-fileloader'], () => {
+gulp.task('browser:parser', ['browser:babel-remove-fileloader'], () => {
 	return gulp
-			.src('./tmp/browser.js')
-			.pipe(browser.browserify())
-			.pipe(rename('sinaps-template.js'))
-			.pipe(gulp.dest('./dist'))
-			.pipe(uglify())
-			.pipe(rename('sinaps-template.min.js'))
-			.pipe(gulp.dest('./dist'))
+			.src('./lib/lexer.pegjs')
+			.pipe(transform(function (filename) {
+				return new Stream.Transform({
+					transform: function (chunk, encoding, done) {
+						var lexer = chunk.toString('utf8');
+						this.push(`module.exports = ` + peg.buildParser(lexer, {
+							cache: false,
+							optimize: 'size',
+							output: 'source'
+						}));
+						done();
+					}
+				});
+			}))
+			.pipe(rename('Parser.js'))
+			.pipe(gulp.dest('./tmp/lib'))
+})
+
+gulp.task('browser:browserify', ['browser:parser'], () => {
+	return browserify('./tmp/main-browser.js')
+		.bundle()
+		.pipe(source('sinaps-template.js'))
+		.pipe(gulp.dest('./dist'))
+		.pipe(streamify(uglify()))
+		.pipe(rename('sinaps-template.min.js'))
+		.pipe(gulp.dest('./dist'))
 });
 
 gulp.task('browser:clean', ['browser:browserify'], () => {
 	return remove(['./tmp']);
 });
 
-gulp.task('default', ['browser:clean']);
+gulp.task('browser', ['browser:clean']);
